e28b5c201fd1b8381cb695252e621e3430c9d26d
Kimberly Baker <bakerk2010@gmail.com>
2020-11-09 22:25:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,9 @@ class EmailAddressParser
   end
 
   def parse
-    
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
   end
     
   email_addresses = (' ')
