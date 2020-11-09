1469477acf79745243e081a0f0661945284a92bd
Kimberly Baker <bakerk2010@gmail.com>
2020-11-09 22:23:53 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,8 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor
+  attr_accessor :email
+  
   
   email_addresses = (' ')
   parser = EmailAddressParser.new(email_addresses)
